5cc830d8f3f88933ea5fe81c478fa0d75cc71a34
maui <hello@mauriciabad.com>
2024-12-26 02:28:19 +0100

    Refetch gameStatus

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,11 @@ import { Main } from './components/Main'
 import { trpc } from './utils/trpc'
 
 export const App: FC = () => {
-  const gameStatus = trpc.game.getStatus.useQuery()
+  const gameStatus = trpc.game.getStatus.useQuery(undefined, {
+    retryDelay: 5_000,
+    retry: Infinity,
+    refetchInterval: 60 * 1_000,
+  })
 
   return gameStatus.data?.status === 'ongoing' ||
     gameStatus.data?.status === 'paused' ? (
