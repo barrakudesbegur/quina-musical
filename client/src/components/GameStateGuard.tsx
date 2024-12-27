7d380043c134eec4eebcdd32c5a9521028156c5b
maui <hello@mauriciabad.com>
2024-12-27 11:35:04 +0100

    Reapply "Better offline mode"
    This reverts commit ff09aa6db5662fefb5a53230f984517c016ac4f1.

--- client/src/components/GameStateGuard.tsx
+++ client/src/components/GameStateGuard.tsx
@@ -6,12 +6,39 @@ import { GameStatus, GameStatusScreen } from './GameStatusScreen'
 export const GameStateGuard: FC<{
   allowedStatuses?: GameStatus[]
 }> = ({ allowedStatuses = ['ongoing'] }) => {
-  const gameStatusSub = trpc.game.getStatus.useSubscription()
+  const gameStatusQuery = trpc.game.getStatusApi.useQuery(undefined, {
+    retryDelay: 5_000,
+    retry: Infinity,
+    refetchInterval: 60 * 1_000,
+  })
+  const utils = trpc.useUtils()
 
-  const gameStatus = useMemo(() => {
+  const gameStatusApi = useMemo(() => {
+    if (gameStatusQuery.failureCount >= 1) return 'not-avilable'
+    if (gameStatusQuery.status === 'success') return gameStatusQuery.data.status
+    if (gameStatusQuery.status === 'pending') return 'loading'
+    return 'not-avilable'
+  }, [gameStatusQuery])
+
+  const gameStatusSub = trpc.game.getStatus.useSubscription(undefined, {
+    onData: async () => {
+      await utils.game.getStatusApi.reset()
+    },
+    onError: async () => {
+      await utils.game.getStatusApi.reset()
+    },
+  })
+
+  const gameStatusFromSub = useMemo(() => {
     if (!gameStatusSub.data) return 'not-avilable'
     return gameStatusSub.data.status
   }, [gameStatusSub.data])
+
+  const gameStatus = useMemo(() => {
+    if (!gameStatusQuery.isSuccess) return gameStatusApi
+
+    return gameStatusFromSub
+  }, [gameStatusApi, gameStatusFromSub, gameStatusQuery.isSuccess])
 
   return allowedStatuses.includes(gameStatus) ? (
     <Outlet />
