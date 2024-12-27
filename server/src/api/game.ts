715e1bf8436c19d998411a2651e518c46bcadb03
maui <hello@mauriciabad.com>
2024-12-27 11:12:09 +0100

    Better offline mode

--- server/src/api/game.ts
+++ server/src/api/game.ts
@@ -6,16 +6,20 @@ import { publicProcedure, router } from '../trpc.js'
 
 const ee = new EventEmitter()
 
-export const gameRouter = router({
-  getStatus: publicProcedure.subscription(async function* ({ signal }) {
-    const getStatus = () => ({
-      status: gameDb.chain.get('finishedAt').value()
-        ? ('finished' as const)
-        : !gameDb.chain.get('currentRound').value()
-          ? ('not-started' as const)
-          : ('ongoing' as const),
-    })
+const getStatus = () => ({
+  status: gameDb.chain.get('finishedAt').value()
+    ? ('finished' as const)
+    : !gameDb.chain.get('currentRound').value()
+      ? ('not-started' as const)
+      : ('ongoing' as const),
+})
 
+export const gameRouter = router({
+  getStatusApi: publicProcedure.query(async () => {
+    return getStatus()
+  }),
+
+  getStatus: publicProcedure.subscription(async function* ({ signal }) {
     yield getStatus()
 
     for await (const [_] of on(ee, 'update', { signal })) {
