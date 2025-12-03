2cb8094fec555a8f0d5d589af7b3c3671eef5d29
maui <hello@mauriciabad.com>
2025-12-04 00:19:09 +0900

    Rename files

--- playlist-fetcher/main.sh
+++ playlist-fetcher/main.sh
@@ -9,6 +9,6 @@ playlist_id="$1"
 python3 fetch.py "$playlist_id"
 
 echo "Downloading songs..."
-spotdl --output "output/songs/{list-position} - {title} - {artist}.{output-ext}" --bitrate disable "https://open.spotify.com/playlist/$playlist_id"
+spotdl --output "output/songs/{list-position} {title} - {artist}.{output-ext}" --bitrate disable "https://open.spotify.com/playlist/$playlist_id"
 
 echo "✅ Done"
