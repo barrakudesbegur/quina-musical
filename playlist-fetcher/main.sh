9092fbf87253b403ce360c1270e1ec53053b86b6
maui <hello@mauriciabad.com>
2025-12-07 20:31:25 +0900

    Also accept playlist url

--- playlist-fetcher/main.sh
+++ playlist-fetcher/main.sh
@@ -1,10 +1,16 @@
 if [[ -z "$1" ]]; then
-  echo "Error: playlist_id is required."
-  echo "Usage: $0 <playlist_id>"
+  echo "Error: playlist_id or playlist URL is required."
+  echo "Usage: $0 <playlist_id|playlist_url>"
   exit 1
 fi
 
-playlist_id="$1"
+input="$1"
+
+if [[ "$input" =~ open\.spotify\.com/playlist/([a-zA-Z0-9]+) ]]; then
+  playlist_id="${BASH_REMATCH[1]}"
+else
+  playlist_id="$input"
+fi
 
 python3 fetch.py "$playlist_id"
 
