7bc47933e4240d92e497494cfdcb0f81010c1d7c
maui <hello@mauriciabad.com>
2025-12-26 12:51:25 +0100

    Change songs json

--- server/src/api/song.ts
+++ server/src/api/song.ts
@@ -4,7 +4,44 @@ import { publicProcedure, router } from '../trpc.js';
 
 export const songRouter = router({
   getAll: publicProcedure.query(async () => {
-    return songs;
+    return songs.map((song) => ({
+      ...song,
+      // TODO: this is temporal until songs are saved in lowdb and already have timestamps
+      timestamps: [
+        {
+          time: 60,
+          tag: 'best',
+          playEffect: {
+            type: 'crossfade',
+            durationSeconds: 1,
+          },
+        },
+        {
+          time: 90,
+          tag: 'main',
+          playEffect: {
+            type: 'crossfade',
+            durationSeconds: 1,
+          },
+        },
+        {
+          time: 0,
+          tag: 'secondary',
+          playEffect: {
+            type: 'crossfade',
+            durationSeconds: 1,
+          },
+        },
+        {
+          time: 150,
+          tag: 'secondary',
+          playEffect: {
+            type: 'crossfade',
+            durationSeconds: 1,
+          },
+        },
+      ],
+    }));
   }),
   getPlaylist: publicProcedure.query(async () => {
     return playlist;
