90d1cd3b089558c969681d12ca85326404bf26be
maui <hello@mauriciabad.com>
2025-12-27 18:03:38 +0100

    Hide images

--- client/src/config/images.ts
+++ client/src/config/images.ts
@@ -6,22 +6,22 @@ export type GalleryImage = {
 };
 
 export const GALLERY_IMAGES: GalleryImage[] = [
-  {
-    id: 'quina-1',
-    label: 'Quina 1',
-    filename: '/images/quina-1.jpeg',
-    discoverable: true,
-  },
-  {
-    id: 'quina-2',
-    label: 'Quina 2',
-    filename: '/images/quina-2.jpeg',
-    discoverable: true,
-  },
-  {
-    id: 'quina-3',
-    label: 'Quina 3',
-    filename: '/images/quina-3.jpeg',
-    discoverable: true,
-  },
+  // {
+  //   id: 'quina-1',
+  //   label: 'Quina 1',
+  //   filename: '/images/quina-1.jpeg',
+  //   discoverable: true,
+  // },
+  // {
+  //   id: 'quina-2',
+  //   label: 'Quina 2',
+  //   filename: '/images/quina-2.jpeg',
+  //   discoverable: true,
+  // },
+  // {
+  //   id: 'quina-3',
+  //   label: 'Quina 3',
+  //   filename: '/images/quina-3.jpeg',
+  //   discoverable: true,
+  // },
 ];
